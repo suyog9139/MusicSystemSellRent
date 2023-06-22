8a3fb79ef5bb18e297066185a043e088e5cd95b3
Tanmay Deshpande <tanmaydeshpande1588@gmail.com>
2023-06-22 23:27:15 +0530

    user and product admin front end done

--- client/src/pages/admin/ProductAdmin.jsx
+++ client/src/pages/admin/ProductAdmin.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios';
-import './User.css'
-const ProductAdmin=()=>{
-    const [myData, setMyData] = useState([]);
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./User.css";
+const ProductAdmin = () => {
+  const [myData, setMyData] = useState([]);
   const [isError, setIsError] = useState("");
 
   // using Async Await
@@ -19,29 +19,44 @@ const ProductAdmin=()=>{
   useEffect(() => {
     getMyProducts();
   }, []);
-    return (
-        <>
-          <div className='heading'>
-    <h1>Products</h1>
-    </div>
-          {isError !== "" && <h2>{isError}</h2>}
-    
-          <div className="grid">
-            {myData.map((post) => {
-              const { Title,Price,Stock,_id} = post;
-              return (
-                <div key={Title} className="card">
 
-                  <h2><strong>Prodict Name :</strong>{Title}</h2>
-                  <h2><strong>Product Cost :</strong> {Price}</h2>
-                  <h2><strong>Quantity: </strong>{Stock}</h2>
-                  <h2><strong>Product Id: </strong>{_id}</h2>
-                </div>
-              );
-            })}
-          </div>
-        </>
-      );
+ 
+  return (
+    <>
+      <div className="heading">
+        <h1>Products</h1>
+      </div>
+      {isError !== "" && <h2>{isError}</h2>}
 
+      <div className="grid">
+        {myData.map((post) => {
+          const { Title, Price, Stock, _id } = post;
+          return (
+            <div key={Title} className="card">
+              <h2>
+                <strong>Prodict Name :</strong>
+                {Title}
+              </h2>
+              <h2>
+                <strong>Product Cost :</strong> {Price}
+              </h2>
+              <h2>
+                <strong>Quantity: </strong>
+                {Stock}
+              </h2>
+              <h2>
+                <strong>Product Id: </strong>
+                {_id}
+              </h2>
+              <div className="heading1">
+                <button  className="button button1">Update product</button>
+                <button  className="button button1">Delete product</button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 };
 export default ProductAdmin;
