b09ef93c1398b5d9cd3a3de7aff9a4848121cb24
suyog9139 <suyogwalunj3@gmail.com>
2023-07-25 21:39:53 +0530

    add product

--- server/routes/product.js
+++ server/routes/product.js
@@ -4,20 +4,29 @@ const router=express.Router()
 import multer from 'multer';
 import {GetAllProducts,ShowCategoryProducts,ShowRentableProduct,ShowPurchaseProduct,AddProduct,DeleteProduct,UpdateProduct} from '../controllers/product.js'
 
-// const storage = multer.diskStorage({
-//     destination: (req, file, cb) => {
-//       cb(null, 'uploads/');
-//     },
-//     filename: (req, file, cb) => {
-//       const fileName =file.originalname;
-//       cb(null, fileName);
-//     },
-//   });
-//   const upload = multer({ storage });
-const upload = multer({ dest: 'Uploads/' });
-// router.route('/').get(GetAllProducts)
-// router.route('/:category').get(ShowCategoryProducts)
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+      const fileName = file.originalname;
+      cb(null, fileName);
+    },
+  });
+  
+  const upload = multer({ storage });
+  
 router.post('/AddProduct', upload.single('Image'), AddProduct);
+// router.post('/AddProduct', upload.single('Image'), function(req, res) {
+//     // Set the headers
+//     res.setHeader('Content-Type', 'application/json');
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+  
+//     // Send the response
+//     res.json({
+//       success: true,
+//     });
+//   });
 router.get("/",GetAllProducts)
 router.get("/:category",ShowCategoryProducts)
 router.get("/:rent",ShowRentableProduct)
